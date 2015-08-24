7235f0d1e8dd2d4d26e57b820c0e47380c4d219e
m-sugawara <m_sugawara@zappallas.com>
2015-08-24 17:00:40 +0900

    bug fix

--- build_ipa.sh
+++ build_ipa.sh
@@ -1,3 +1,3 @@
-PROVISIONING_FILE_NAME=ios profiles:dowload -u "${IOS_DEV_EMAIL}" -p "${IOS_DEV_PASSWORD}" "${IOS_DEV_PROVISIONING_NAME}" | sed -e "s/Successfully downloaded: '\(.*\)'/\1/g"
+PROVISIONING_FILE_NAME=ios profiles:download -u "${IOS_DEV_EMAIL}" -p "${IOS_DEV_PASSWORD}" "${IOS_DEV_PROVISIONING_NAME}" | sed -e "s/Successfully downloaded: '\(.*\)'/\1/g"
 ipa build --configuration ${BUILD_CONFIG} --scheme ${SCHEME} CODE_SIGN_IDENTITY='iPhoneDevelper'
 ipa distribute:crashlytics -c ./Pods/Crashlytics/Crashlytics.framework -a "${FABRIC_API_TOKEN}" -s "${FABRIC_BUILD_SECRET}"

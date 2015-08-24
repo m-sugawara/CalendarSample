10ec2ced57adc85cd257ad36c7df28f643bc2b62
m-sugawara <m_sugawara@zappallas.com>
2015-08-24 18:35:04 +0900

    add encrypted cert

--- build_ipa.sh
+++
@@ -1,6 +0,0 @@
-PROVISIONING_FILE_NAME= ios profiles:download --team "${IOS_DEV_TEAM}" -u "${IOS_DEV_EMAIL}" -p "${IOS_DEV_PASSWORD}" "${IOS_DEV_PROVISIONING_NAME}" | sed -e "s/Successfully downloaded: '\(.*\)'/\1/g"
-chown :wheel /Library/Developer/CoreSimulator/Profiles/Runtimes/iOS\ *.simruntime/Contents/Resources/RuntimeRoot/usr/lib/dyld_sim
-xcode-select --print-path
-xcode-select --switch /Applications/XCode.app/Contents/Developer
-ipa build --configuration ${BUILD_CONFIG} --scheme ${SCHEME} CODE_SIGN_IDENTITY='iPhoneDevelper'
-ipa distribute:crashlytics -c ./Pods/Crashlytics/Crashlytics.framework -a "${FABRIC_API_TOKEN}" -s "${FABRIC_BUILD_SECRET}"

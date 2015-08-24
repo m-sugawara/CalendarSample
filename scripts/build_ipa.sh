923ba9d2cc1b2da5fcbcab1b910c3b0c9b4f1326
m-sugawara <m_sugawara@zappallas.com>
2015-08-24 19:23:19 +0900

    fix script bug

--- scripts/build_ipa.sh
+++ scripts/build_ipa.sh
@@ -2,7 +2,7 @@
 PROVISIONING_FILE_NAME= ios profiles:download -u "${IOS_DEV_EMAIL}" -p "${IOS_DEV_PASSWORD}" --team "ZAPPALLAS, INC." "${IOS_DEV_PROVISIONING_NAME}" | sed -e "s/Successfully downloaded: '\(.*\)'/\1/g"
 PROVISIONING_FILE_NAME=${IOS_DEV_PROVISIONING_NAME}".movileProvision"
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "./"${IOS_DEV_PROVISIONING_NAME} ~/Library/MobileDevice/Provisioning\ Profiles/
+cp "./"${PROVISIONING_FILE_NAME} ~/Library/MobileDevice/Provisioning\ Profiles/
 
 # ipaファイルを作成
 ipa build --embed ${PROVISIONING_FILE_NAME} --configuration ${BUILD_CONFIG} --scheme ${SCHEME} CODE_SIGN_IDENTITY='iPhoneDevelper'

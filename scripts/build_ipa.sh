4299b29ca6ba53faff46242f012c451b9f2a0d10
m-sugawara <m_sugawara@zappallas.com>
2015-08-24 18:55:30 +0900

    change team

--- scripts/build_ipa.sh
+++ scripts/build_ipa.sh
@@ -1,4 +1,4 @@
-PROVISIONING_FILE_NAME= ios profiles:download -u "${IOS_DEV_EMAIL}" -p "${IOS_DEV_PASSWORD}" --team "${IOS_DEV_TEAM}" "${IOS_DEV_PROVISIONING_NAME}" | sed -e "s/Successfully downloaded: '\(.*\)'/\1/g"
+PROVISIONING_FILE_NAME= ios profiles:download -u "${IOS_DEV_EMAIL}" -p "${IOS_DEV_PASSWORD}" --team "ZAPPALLAS, INC." "${IOS_DEV_PROVISIONING_NAME}" | sed -e "s/Successfully downloaded: '\(.*\)'/\1/g"
 
 ipa build --embed ${PROVISIONING_FILE_NAME} --configuration ${BUILD_CONFIG} --scheme ${SCHEME} CODE_SIGN_IDENTITY='iPhoneDevelper'
 ipa distribute:crashlytics -c ./Pods/Crashlytics/Crashlytics.framework -a "${FABRIC_API_TOKEN}" -s "${FABRIC_BUILD_SECRET}"

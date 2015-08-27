02814e0d5f171299c2e097a19a25ffe03db2fdd9
m-sugawara <m_sugawara@zappallas.com>
2015-08-27 11:13:00 +0900

    test3

--- scripts/circle/build_ipa.sh
+++ scripts/circle/build_ipa.sh
@@ -1,10 +1,10 @@
 # deployment/testブランチにコミットした時だけIPAを作成します。
 #!/bin/sh
-echo $CI_PULL_REQUESTS
-echo $CI_PULL_REQUEST
-echo $CIRCLE_BRANCH
-echo $CIRCLE_PR_REPONAME
-echo $CIRCLE_PR_NUMBER
+echo "$CI_PULL_REQUESTS"
+echo "$CI_PULL_REQUEST"
+echo "$CIRCLE_BRANCH"
+echo "$CIRCLE_PR_REPONAME"
+echo "$CIRCLE_PR_NUMBER"
 
 if [[ "$CI_PULL_REQUEST" != "false" ]]; then
 	exit 0
